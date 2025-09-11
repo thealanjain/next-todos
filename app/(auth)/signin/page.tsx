976c679e06b06e3e813b017b838b514da21602ce
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-09-11 14:52:50 +0530

    Restructured UI

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,35 +1,40 @@
 import Link from "next/link"
-import { Col, Form, Row } from "react-bootstrap"
+import { Button, Col, Form, Row } from "react-bootstrap"
 
 const SigninPage = () => {
     return (
-        <>
-            <Row>
-                <Col className="col-12">
-                    <h1>Sign In</h1>
-                    <div className="d-flex justify-content-between">
-                        <p>
+        <Row className="justify-content-center">
+            <Col className="col-12 w-50">
+                <Row>
+                    <Col className="col-12 mb-3">
+                        <h1>Sign In</h1>
+                    </Col>
+                    <Col className="col-12 bg-light p-3 rounded mb-3">
+                        <Form>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="name@example.com"
+                            />
+                            <Form.Label className="mt-3">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form>
+                        <Button className="mt-3" variant="primary">
+                            Sign In
+                        </Button>
+                    </Col>
+                    <Col className="col-12">
+                        <p className="">
                             If you don't have an account, please{" "}
                             <Link href="/signup">sign up.</Link>
                         </p>
-                        <Link href="/">Home</Link>
-                    </div>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="col-12 bg-light p-3 rounded">
-                    <Form>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="name@example.com"
-                        />
-                        <Form.Label className="mt-3">Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form>
-                </Col>
-            </Row>
-        </>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
     )
 }
 
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,42 +1,45 @@
 import Link from "next/link"
-import { Col, Form, Row } from "react-bootstrap"
+import { Button, Col, Form, Row } from "react-bootstrap"
 
 const SignupPage = () => {
     return (
-        <>
-            <Row>
-                <Col className="col-12">
-                    <h1>Sign Up</h1>
-                    <div className="d-flex justify-content-between">
-                        <p>
+        <Row className="justify-content-center">
+            <Col className="col-12 w-50">
+                <Row>
+                    <Col className="col-12 mb-3">
+                        <h1>Sign Up</h1>
+                    </Col>
+                    <Col className="col-12 bg-light p-3 rounded mb-3">
+                        <Form>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="name@example.com"
+                            />
+                            <Form.Label className="mt-3">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                            />
+                            <Form.Label className="mt-3">
+                                Confirm Password
+                            </Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirm Password"
+                            />
+                        </Form>
+												<Button className="mt-3" variant="primary">Sign Up</Button>
+                    </Col>
+                    <Col className="col-12">
+                        <p className="">
                             If you already have an account, please{" "}
                             <Link href="/signin">sign in.</Link>
                         </p>
-                        <Link href="/">Home</Link>
-                    </div>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="col-12 bg-light p-3 rounded">
-                    <Form>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="name@example.com"
-                        />
-                        <Form.Label className="mt-3">Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                        <Form.Label className="mt-3">
-                            Confirm Password
-                        </Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Confirm Password"
-                        />
-                    </Form>
-                </Col>
-            </Row>
-        </>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
     )
 }
 
